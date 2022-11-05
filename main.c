144772888296b63dc63701343007ccd79b9249fb
Gafar H. Salam <aygafsam2013@gmail.com>
2022-11-05 14:10:01 +0100

    Updating main file by adding student record function

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h>
 
 //Function
 void addStudent();
@@ -10,13 +9,14 @@ void deleteStudent();
 //Structure
 struct student{
     char regNo[20];
-    char firs_name[50];
+    char first_name[50];
     char last_name[50];
     char dept[100];
     char email[100];
-    int phone;
-    char level[10];
+    int phone[11];
+    char level[5];
 };
+/**Main Function */
 void main()
 {
     int choice;
@@ -33,28 +33,35 @@ void main()
         scanf("%d", &choice);
 
         switch(choice){
-    case 1:
-        system("cls");
-        addStudent();
-        system("cls");
-        break;
+        case 1:
+            system("cls");
+            addStudent();
+            system("cls");
+            break;
+        case 2:
+            system("cls");
+            studentRecord();
+            printf("\t\t\tPress any key to EXIT\n");
+            getch();
+            system("cls");
+            break;
         }
     }
 }
 
 void addStudent(){
-    char another;
+    char add;
     FILE *fp;
     struct student info;
 
     do{
         system("cls");
         printf("\t\t\t\t--------Add Student Info---------\n\n\n");
-        fp = fopen("Student_info.txt","a");
+        fp = fopen("student_info.txt","a");
         printf("\n\t\t\tEnter Registration Number : ");
         scanf("%s", &info.regNo);
         printf("\n\t\t\tEnter First name : ");
-        scanf("%s", &info.firs_name);
+        scanf("%s", &info.first_name);
         printf("\n\t\t\tEnter Last Name : ");
         scanf("%s", &info.last_name);
         printf("\n\t\t\tEnter Department : ");
@@ -65,19 +72,45 @@ void addStudent(){
         scanf("%s", &info.email);
         printf("\n\t\t\tEnter Phone Number : ");
         scanf("%i", &info.phone);
-        printf("\n\t\t\t______________________________\n");
+        printf("\n\t\t\t__________________________\n");
 
         if(fp==NULL){
             fprintf(stderr,"\t\t\t Can't open file\n");
         }else{
-            printf("\t\t\tStudent Record Stored Successfully.\n");
+            printf("\t\t\t Student Record Stored Successfully.\n");
         }
 
-        fwrite(&info,sizeof(struct student),1,fp);
+        fwrite(&info, sizeof(struct student),1,fp);
         fclose(fp);
 
-        printf("\t\t\tDo you want to add another record?(y/n) : ");
-        scanf("&c",&another);
+        printf("\t\t\t Do you want to add another record?(y/n) : ");
+        scanf("&s",&add);
 
-    }while(another=='y' || another=='Y');
+    }while(add=='y' || add=='Y');
+}
+
+void studentRecord(){
+    FILE *fp;
+    struct student info;
+    fp = fopen("student_info.txt", "r");
+
+    printf("\t\t\t\t----------Student Records------------\n\n\n");
+
+    if(fp==NULL){
+        fprintf(stderr, "\t\t\t\tCan't open file\n");
+    }else{
+            printf("\t\t\t\tStudent Records\n");
+            printf("\t\t\t\t________________\n\n");
+        }
+    while(fread(&info, sizeof(struct student),1,fp)){
+        printf("\n\t\t\t\tStudent Registration Number : %s", info.regNo);
+        printf("\n\t\t\t\tStudent FullName : %s %s", info.first_name, info.last_name);
+        printf("\n\t\t\t\tStudent Department : %s", info.dept);
+        printf("\n\t\t\t\tStudent Level : %s", info.level);
+        printf("\n\t\t\t\tStudent Email Address : %s", info.email);
+        printf("\n\t\t\t\tStudent Phone Number : %i", info.phone);
+        printf("\n\t\t\t\t_______________________________\n");
+    }
+    fclose(fp);
+    getch();
 }
